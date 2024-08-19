87b6b0969c5e3cd9c8fb6a5097ebf59460b3efe2
unknown <luvaa30@gmail.com>
2024-08-19 18:50:04 -0300

    produccion local

--- notes-back/index.js
+++ notes-back/index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 
+app.use(express.static('dist'))
 app.use(cors())
 app.use(express.json())
 
@@ -24,9 +25,9 @@ let notes = [
     }
 ]
 
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
-})
+// app.get('/', (request, response) => {
+//     response.send('<h1>Hello World!</h1>')
+// })
 
 app.get('/api/notes', (request, response) => {
     response.json(notes)
